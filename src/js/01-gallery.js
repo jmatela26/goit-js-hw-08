4fc1b9255b74960a8d83d5ce2fcec46a284cfabe
jmatela26 <jmatela26@gmail.com>
2024-01-25 23:18:06 +0800

    Added fixes for the java script

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
+const createItemsMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+      `;
+  })
+  .join('');
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.insertAdjacentHTML('beforeend', createItemsMarkup);
+let lightbox = new SimpleLightbox('.gallery a', {
+    scrollZoom: false,
+    captionDelay: 250,
+    captionsData: 'alt',
+    doubleTapZoom: 1,
+  });
+  
+  galleryContainer.addEventListener('click', event => {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+      return;
+    }
+  });
 console.log(galleryItems);
